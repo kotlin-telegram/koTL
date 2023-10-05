120476052c429f65e29f0338f60ab1fb0faa9653
Alex Sokol / y9san9 <y9san9@gmail.com>
2023-10-05 13:42:56 +0300

    fix: workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,3 @@
 plugins {
-    id("kmp-library-convention")
-    id("publication-convention")
     id("print-sdk-version-convention")
-}
-
-version = libs.versions.koTLVersion.get()
-
-dependencies {
-    commonMainImplementation(libs.kotlinxSerialization)
 }
