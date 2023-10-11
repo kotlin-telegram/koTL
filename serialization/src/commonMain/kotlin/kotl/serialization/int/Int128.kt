9c9f7a681e6a33b11730f2641900bbd9d23e9b3f
Alex Sokol / y9san9 <y9san9@gmail.com>
2023-10-11 13:44:03 +0300

    fix: fixed Int128 serialization

--- serialization/src/commonMain/kotlin/kotl/serialization/int/Int128.kt
+++ serialization/src/commonMain/kotlin/kotl/serialization/int/Int128.kt
@@ -1,10 +1,15 @@
 package kotl.serialization.int
 
 import kotl.serialization.annotation.TLSize
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlin.jvm.JvmInline
 
-@Serializable
+@Serializable(with = Int128.Serializer::class)
 public data class Int128(
     @TLSize(bits = 128)
     public val data: IntArray
@@ -22,4 +27,35 @@ public data class Int128(
     override fun hashCode(): Int {
         return data.contentHashCode()
     }
+
+    public object Serializer : KSerializer<Int128> {
+
+        @OptIn(ExperimentalSerializationApi::class)
+        override val descriptor: SerialDescriptor = SerialDescriptor(
+            serialName = "kotl.serialization.int.Int128",
+            original = Int128Serializable.serializer().descriptor
+        )
+
+        override fun deserialize(decoder: Decoder): Int128 {
+            val serializable = decoder.decodeSerializableValue(
+                deserializer = Int128Serializable.serializer()
+            )
+            return Int128(serializable.data)
+        }
+
+        override fun serialize(encoder: Encoder, value: Int128) {
+            val serializable = Int128Serializable(value.data)
+            encoder.encodeSerializableValue(
+                serializer = Int128Serializable.serializer(),
+                value = serializable
+            )
+        }
+    }
 }
+
+@Serializable
+@JvmInline
+private value class Int128Serializable(
+    @TLSize(bits = 128)
+    val data: IntArray
+)
